ab40f63ae6d32467ecac9e74653d98b620c26e53
Adryane Gonçalves <adryane2109@usp.br>
2026-09-23 11:08:52 -0300

    resolvendo exercicio 13

--- exerc_13.py
+++ exerc_13.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-
 bases_nitrogenadas = ['ATGCCCGGCCCGGC', 'GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT', 'ATGGGCCC']
 
-tuplas = []
+tuplas = [(i, len(DNA), DNA) for i, DNA in enumerate(bases_nitrogenadas, start=1)]
 
-for DNA in bases_nitrogenadas:
-    tupla = (len(DNA), DNA)
-    tuplas.append(tupla)
-
-for numero, (comprimento, DNA) in enumerate(tuplas, start=1):
+for numero, comprimento, DNA in tuplas:
     print(numero, comprimento, DNA, sep="\t")
