cb412d1ee3cb85fa9b981035b6982efbb2c1893c
Adryane Gonçalves <adryane2109@usp.br>
2026-09-24 17:29:09 -0300

    resolvendo exercicio 11

--- exerc_11.py
+++ exerc_11.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 bases_nitrogenadas = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
+print(bases_nitrogenadas)
 for dna in bases_nitrogenadas:
 	print(len(dna), dna, sep="\t")
