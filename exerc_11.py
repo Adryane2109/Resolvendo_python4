f04822e9f20749d940b50b17efb5667dfa639399
Adryane Gonçalves <adryane2109@usp.br>
2026-09-24 17:33:41 -0300

    resolvendo exercicio 11

--- exerc_11.py
+++ exerc_11.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 bases_nitrogenadas = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
+
+#print de cada elemento:
 print(bases_nitrogenadas)
+
+#imprimindo o comprimento e a sequência (separados por uma guia):
 for dna in bases_nitrogenadas:
 	print(len(dna), dna, sep="\t")
