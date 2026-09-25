1cc99f9fcf6199e27a82eeb84edb179a381639be
Adryane Gonçalves <adryane2109@usp.br>
2026-09-25 09:34:44 -0300

    resolvendo exercicio 10

--- exerc_10.py
+++ exerc_10.py
@@ -4,7 +4,11 @@ import sys
 inicio = int(sys.argv[1])
 fim = int(sys.argv[2])
 
-numeros = [i for i in range(inicio, fim + 1) if i % 2 != 0]
+numeros = [i for i in range(inicio, fim + 1)]
 
+print(numeros)
+
+# parte 2 do exercício:
 for numero in numeros:
-    print(numero)
+    if numero % 2 != 0:
+        print(numero)
